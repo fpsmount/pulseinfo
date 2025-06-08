c953234469b999785b424e4a75328bd06a76c7ce
Dimitri Monteiro <62824846+fpsmount@users.noreply.github.com>
2025-06-08 13:23:19 -0300

    Add files via upload

---
+++ pulseinfo/src/main/java/br/com/fpsmount/pulseinfo/PulseinfoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fpsmount.pulseinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseinfoApplication.class, args);
+	}
+
+}
---
+++ pulseinfo/src/test/java/br/com/fpsmount/pulseinfo/PulseinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fpsmount.pulseinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
